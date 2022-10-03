3b808d3e14a423b4c96b0a5f6a18d1e84e9688d4
renrevm <109566216+renrevm@users.noreply.github.com>
2022-10-03 13:49:55 -0300

    arreglo

--- modelos/formularios.modelo.php
+++ modelos/formularios.modelo.php
@@ -25,13 +25,11 @@ class ModeloFormularios{
     */
     static public function mdlSeleccionarRegistros($tabla, $item, $valor){
         if($item == null && $valor == null){
-            $stmt = Conexion::conectar()->prepare("SELECT *, DATE_FORMAT(fecha, '%d/%m/%Y') AS fecha
-            FROM $tabla ORDER BY id DESC");
+            $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla ORDER BY id DESC");
             $stmt->execute();
             return $stmt -> fetchAll();    
         }else{
-            $stmt = Conexion::conectar()->prepare("SELECT *, DATE_FORMAT(fecha, '%d/%m/%Y') AS fecha
-            FROM $tabla WHERE $item = :$item ORDER BY id DESC");
+            $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item ORDER BY id DESC");
             $stmt-> bindParam(":".$item,$valor, PDO::PARAM_STR);
             $stmt->execute();
             return $stmt -> fetch();    
