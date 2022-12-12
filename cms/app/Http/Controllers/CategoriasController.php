91675146d4430840842fb127a59a855ddea5e936
Tunncho <gonzalo.alvarezv@usm.cl>
2022-12-11 23:40:18 -0300

    controller categoria,producto

---
+++ cms/app/Http/Controllers/CategoriasController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Categoria::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $producto = new Categoria();
+        $producto->id = $request->id;
+        $producto->Nombre_categoria = $request->nombre_categoria;
+        $producto->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Categoria  $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Categoria $categoria)
+    {
+        return $categoria;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Categoria  $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Categoria $categoria)
+    {
+        $producto->id = $request->id;
+        $producto->Nombre_categoria = $request->nombre_categoria;
+        $producto->save();
+        return $categoria;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Categoria  $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Categoria $categoria)
+    {
+        $categoria->delete();
+    }
+}
---
+++ cms/app/Http/Controllers/ProductosController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Producto;
+use Illuminate\Http\Request;
+
+class ProductosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Producto::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //dd($request->cod_producto);
+        $producto = new Producto();
+        $producto->id = $request->id;
+        $producto->id_categoria = $request->id_categoria;
+        $producto->Nombre_producto = $request->nombre_producto;
+        $producto->precio_costo = $request->precio_costo;
+        $producto->precio_venta = $request->precio_venta;
+        $producto->stock_disponible = $request->stock_disponible;
+        $producto->save();
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Producto $producto)
+    {
+        return $producto;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Producto $producto)
+    {
+        $producto->id = $request->id;
+        $producto->id_categoria = $request->id_categoria;
+        $producto->Nombre_producto = $request->nombre_producto;
+        $producto->precio_costo = $request->precio_costo;
+        $producto->precio_venta = $request->precio_venta;
+        $producto->stock_disponible = $request->stock_disponible;
+        $producto->save();
+        return $producto;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Producto $producto)
+    {
+        $producto->delete();
+    }
+}
